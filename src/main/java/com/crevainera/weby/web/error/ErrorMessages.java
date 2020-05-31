6e4b945418d86239292067d0ed86d9cb338861e2
Cristian Revainera <cristian.revainera@gmail.com>
2020-05-31 12:12:31 -0300

    Updates error messages constants'

--- src/main/java/com/crevainera/weby/web/error/ErrorMessages.java
+++ src/main/java/com/crevainera/weby/web/error/ErrorMessages.java
@@ -9,7 +9,9 @@ public enum ErrorMessages {
 
     ENDPOINT_WE_ARE_WORKING("Internal Server Error. We are working to fix it."),
     ENDPOINT_VALIDATION_ERROR("Validation Error"),
-    ENDPOINT_INTERNAL_SERVER_ERROR("Internal Server Error");
+    ENDPOINT_INTERNAL_SERVER_ERROR("Internal Server Error"),
+
+    SERVICE_DEFAULT_IMAGE_NOT_FOUND("Default news image resource not found");
 
     private String message;
 }

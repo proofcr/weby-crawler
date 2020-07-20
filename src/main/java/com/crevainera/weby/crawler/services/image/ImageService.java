a200f3fc74413358894c72dd0173929efc6512f3
Cristian Revainera <cristian.revainera@gmail.com>
2020-07-19 21:04:33 -0300

    Updates ImageService

--- src/main/java/com/crevainera/weby/crawler/services/image/ImageService.java
+++ src/main/java/com/crevainera/weby/crawler/services/image/ImageService.java
@@ -4,6 +4,7 @@ import com.crevainera.weby.crawler.entities.Article;
 import com.crevainera.weby.crawler.exception.WebyException;
 import com.crevainera.weby.crawler.repositories.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.net.MalformedURLException;
@@ -16,6 +17,7 @@ public class ImageService {
     private ArticleRepository articleRepository;
     private ThumbService thumbService;
 
+    @Autowired
     public ImageService(final ArticleRepository articleRepository, final ThumbService thumbService) {
         this.articleRepository = articleRepository;
         this.thumbService = thumbService;

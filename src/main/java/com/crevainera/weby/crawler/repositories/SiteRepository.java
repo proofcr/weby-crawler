01aa579e537f1f129d11e36abe702ffb1d6a50e6
Cristian Revainera <cristian.revainera@gmail.com>
2020-10-17 19:43:34 -0300

    Updates SiteRepository interface

--- src/main/java/com/crevainera/weby/crawler/repositories/SiteRepository.java
+++ src/main/java/com/crevainera/weby/crawler/repositories/SiteRepository.java
@@ -4,8 +4,7 @@ import com.crevainera.weby.crawler.entities.Site;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface SiteRepository extends CrudRepository<Site, Long> {
-    Optional<List<Site>> findByEnabledTrue();
+    List<Site> findByEnabledTrue();
 }
